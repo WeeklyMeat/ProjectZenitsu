a7d3bf444a8459a9204286adccc23685549c1962
WeeklyMeat <55235916+WeeklyMeat@users.noreply.github.com>
2020-02-17 19:11:06 +0100

    Add Login system
    Change login to work :)

--- src/Login.php
+++ src/Login.php
@@ -2,11 +2,12 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Project Zenitsu</title>
+    <title>Login</title>
     <meta name="description" content="bla">
     <meta name="author" content="WeeklyMeat">
     <link rel="stylesheet" type="text/css" href="style/Main.css">
-    <!-- <link rel="stylesheet" type="text/css" href="style/darkmode.css"> -->
+    <link rel="stylesheet" type="text/css" href="style/Forms.css">
+    <!-- <link rel="stylesheet" type="text/css" href="style/Darkmode.css"> -->
     <link href="https://fonts.googleapis.com/css?family=Quicksand&display=swap" rel="stylesheet">
 </head>
 <?php
@@ -14,25 +15,41 @@
     if(!isset($_SESSION))
         session_start();
 
-    if(isset($_SESSION["user"]))
-        header("Location: Index.php?mode=feed");
-
     $dbc = new DatabaseConnection();
     $userContr = new UserContr(new UserModel($dbc));
+
 ?>
 <body>
     <div class = "sidebar" id = "sidebar_left">
         <nav class="navbar">
             <ul class="nav-list"><?php
-                    
-                NavbarView::outputNavOptionsLoggedOut();
-                $userContr->login("WeeklyMeat", "1234");
+
+                if(isset($_SESSION["user"]))
+                    header("Location: Index.php?mode=feed");
+
+                else
+                    NavbarView::outputNavOptionsLoggedOut();
             ?>
             </ul>
         </nav>
     </div>
     <section id = "content">
+<?php       // PHP section
 
+            if(isset($_POST["username"]) && $username = trim(htmlspecialchars($_POST["username"])))
+                    if(isset($_POST["password"]) && $password = trim(htmlspecialchars($_POST["password"])))
+                        $isLoggedIn = $userContr->login($username, $password);
+
+            if($isLoggedIn ?? false)
+                header("Location: Index.php?mode=feed");
+            else
+                $error = true;
+?>
+        <form action="Login.php" method="POST">
+            <div class="formDiv"><input type="text" name="username" class="form" placeholder="Username"></div>
+            <div class="formDiv"><input type="password" name="password" class="form" placeholder="Password"></div>
+            <input type="submit" name="submit" id="submit">
+        </form>
     </section>
     <div class = "sidebar" id = "sidebar_right">
     </div>
